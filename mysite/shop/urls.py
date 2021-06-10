444b57b71fb23c3c0847c9e115faee5c95d1f0b1
mina199800 <mina.khosravi1998@gmail.com>
2021-06-10 14:48:20 -0500

    chapter2

--- mysite/shop/urls.py
+++ mysite/shop/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import shop_list
+from .views import post_list
 
 app_name = 'shop'
 urlpatterns = [
 # post views
-    path('', shop_list, name='shop_list'),
+    path('', post_list, name='shop_list'),
 #     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
 #      post_detail,
 #      name='post_detail'),
--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,7 +1,35 @@
-from .models import shop
 from django.shortcuts import render, get_object_or_404
+from .models import shop
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-def shop_list(request):
-    posts_all = shop.objects.all()
-    return render(request,'shop/post/index.html',{'posts': posts_all})
+def post_list(request):
+    object_list = shop.objects.filter(status='published')
+    paginator = Paginator(object_list, 2) # 2 posts in each page
+    try :
+        page = request.GET.get('page')
+    except :
+        page=1
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+    # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    
+    return render(request, 'shop/post/index.html',
+                        {'page': page,
+                        'posts': posts})
 
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(shop, slug=post,
+            status='published',
+            publish__year=year,
+            publish__month=month,
+            publish__day=day)
+    post.visits = post.visits +1 
+    post.save()
+    return render(request,
+    'shop/post/detail.html',
+    {'post': post})
