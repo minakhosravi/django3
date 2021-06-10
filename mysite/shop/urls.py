7791965a33cac0261c11dc6cbf82aaa5382d1618
mina199800 <mina.khosravi1998@gmail.com>
2021-06-10 15:09:40 -0500

    urls

--- mysite/shop/urls.py
+++ mysite/shop/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list, post_detail
 
 app_name = 'shop'
 urlpatterns = [
 # post views
     path('', post_list, name='shop_list'),
-#     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-#      post_detail,
-#      name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+    post_detail,
+    name='post_detail'),
  ]
